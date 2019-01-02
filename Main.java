54e16666e96662dc68ab20fed3dc992cd920ea81
yun <cjswo9207@gmail.com>
2019-01-02 15:40:47 +0900

    test

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 ﻿public class Main           {   
 public static void main(String[] args)          {
-System.out.println("dfdfdf");// 이슈해결
-System.out.println("bug complete");
+    System.out.println("dfdfdf");// 이슈해결
+    System.out.println("bug complete");
+        System.out.println("bug complete2");
 		          }
      }
