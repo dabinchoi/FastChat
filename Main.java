5b49df7a09c1d6032435d06f0a594003dd0fd5bf
jungmisu <jungmisu801@gmail.com>
2019-01-02 15:40:20 +0900

    conflict1

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 ﻿public class Main           {   
 public static void main(String[] args)          {
 System.out.println("dfdfdf");// 이슈해결
-System.out.println("bug complete");
+System.out.println("bug complete conflict?");
 		          }
      }
